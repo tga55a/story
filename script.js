3e6c7e359ea0dfda7b5645aba16a4c8a9f684c57
tga55a <tgardner2@my.waketech.edu>
2025-02-26 14:00:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-function Introduction() {
-    window.addEventListener("keydown", () => {
-        console.log("Proceed")
-        goToScene("Scene1")
-    })
+
+
+let desc = document.getElementById("story")
+
+if (desc) {
+    Typewrite(desc.innerHTML, desc)
+}
+
+function Typewrite(message, element) {
+    console.log("BRUH")
+    let i = 0
+    element.innerHTML = ""
+    function type() {
+        if (i < message.length) {
+            setTimeout(type, 50);
+            element.innerHTML= element.innerHTML + message.charAt(i)
+            i++
+        }
+    }
+    type()
 }
 
 
 function goToScene(scene) {
+    console.log("SCENE")
     window.location.href = scene + ".html"; 
+
+
+    let desc = document.getElementById("story")
+    desc.innerHTML = "ok bro"
+    // Typewrite(desc.innerHTML, desc)
 }
